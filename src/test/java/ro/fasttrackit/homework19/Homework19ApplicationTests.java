1d75470188430c526894104ceac4a03fa97b9aea
Andrei Oprea <opreaandrew@yahoo.com>
2023-11-14 05:25:35 +0200

    Started

---
+++ src/test/java/ro/fasttrackit/homework19/Homework19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.homework19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
